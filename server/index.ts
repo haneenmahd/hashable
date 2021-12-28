af36e1bdc0bc503658a192aa2278d033a7c65b35
Haneen Mahdin <haneenmahdin@gmail.com>
2021-12-28 20:00:18 +0530

    Integrate hashing with server

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import open from "open";
 
+// data types ⦂
+import { Request } from "./src/types";
+
+// utility functions to convert string values to enums
+import { convertAlgo, convertEncoding } from "./utils/convert";
+
+// cryptographic functions
+import hash from "./src/hash";
+
 const app = express();
 const PORT: string | number = process.env.PORT || 3000;
 
@@ -8,10 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.send("Listening and watching out for crypto functions 👀");
+});
+
+app.get("/api/hash", (req, res) => {
+    const reqObj: Request = {
+      algorithm: convertAlgo(String(req.query.algorithm)),
+      str: String(req.query.str),
+      encoding: convertEncoding(String(req.query.encoding)),
+    };
+
+    res.send(hash(reqObj));
 });
 
 // opens and listens at a specific url
 open(`http://localhost:${PORT}`);
-console.log(`Listening and serving on PORT: ${PORT}`)
+console.log(`Listening and serving on PORT: ${PORT}`);
 app.listen(PORT);
