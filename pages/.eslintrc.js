77104b6b866e4fd71eed6dc11447c23da0c94803
Haneen Mahdin <haneenmahdin@gmail.com>
2021-12-28 22:51:49 +0530

    `Config` eslint for pages

---
+++ pages/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 13
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
