e57e8e69ae470b7d02b0321e43bf3256ce104704
Haneen Mahdin <haneenmahdin@gmail.com>
2021-12-28 20:21:43 +0530

    `src 🛠`: add hmac functions

---
+++ server/src/hmac.ts
@@ -0,0 +1,51 @@
+import crypto, { BinaryToTextEncoding } from "crypto";
+import { Encoding, Algorithm, HmacRequest } from "./types";
+
+function getAlgorithmType(algorithm: Algorithm): string {
+  const algo = algorithm;
+
+  switch (algo) {
+    case Algorithm.SHA256:
+      return "sha256";
+
+    case Algorithm.SHA512:
+      return "sha512";
+
+    case Algorithm.MD5:
+      return "MD5";
+
+    default:
+      return "sha256";
+  }
+}
+
+function getEncoding(encoding: Encoding): BinaryToTextEncoding {
+  const enc = encoding;
+
+  switch (enc) {
+    case Encoding.HEX:
+      return "hex";
+
+    case Encoding.BASE64:
+      return "base64";
+
+    case Encoding.BASE64URL:
+      return "base64url";
+
+    default:
+      return "hex";
+  }
+}
+
+export default function hmac(req: HmacRequest) {
+  // retrieving values from enums
+  const algorithmType = getAlgorithmType(req.algorithm);
+  const encodingType: BinaryToTextEncoding = getEncoding(req.encoding);
+
+  const hmac = crypto
+    .createHmac(algorithmType, req.key)
+    .update(req.str)
+    .digest(encodingType);
+
+  return hmac;
+}
