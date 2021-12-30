4da0f8bc9c008eccaaa62f1c069c3c1274525073
Haneen Mahdin <haneenmahdin@gmail.com>
2021-12-30 16:24:14 +0530

    comlete api page

---
+++ web/scripts/api/index.js
@@ -0,0 +1,20 @@
+const links = document.querySelectorAll("div.api-nav .api-link");
+const contentHeader = document.getElementById("content-header");
+const contentSecondaryHeader = document.getElementById("content-secondary-header");
+const contentDescription = document.getElementById("content-description");
+
+contentHeader.innerText = links[0].getAttribute("data-header");
+contentSecondaryHeader.innerText = links[0].getAttribute("data-secondary-header");
+contentDescription.innerText = links[0].getAttribute("data-description");
+
+links.forEach(link => {
+    link.onclick = () => {
+        document.querySelectorAll("div.api-nav .api-link.active")[0].classList.remove("active");
+
+        link.classList.add("active");
+
+        contentHeader.innerText = link.getAttribute("data-header");
+        contentSecondaryHeader.innerText = link.getAttribute("data-secondary-header");
+        contentDescription.innerText = link.getAttribute("data-description");
+    };
+});
