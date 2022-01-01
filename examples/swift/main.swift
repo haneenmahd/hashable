2049261aaf8e56383bb3b4521889df82094c1017
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-01 14:50:50 +0530

    Add Example for swift

---
+++ examples/swift/main.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let url = URL(string: "https://hashable-server.herokuapp.com/api/hash?algorithm=sha256&str=HelloWorld&encoding=hex")!
+
+let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+    guard let data = data else { return }
+    print(String(data: data, encoding: .utf8)!)
+}
+
+task.resume()
