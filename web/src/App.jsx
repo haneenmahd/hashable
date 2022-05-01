82498ead257f8b01909ae2157c773ef8a7eff71d
Haneen Mahdin <haneenmahdin@gmail.com>
2022-05-01 21:37:16 +0530

    Add new simple and clean design

---
+++ web/src/App.jsx
@@ -0,0 +1,31 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <span class={styles.titles}>
+          <h3>Light.</h3>
+          <h3>Open Source.</h3>
+          <h3>Simple and Easy.</h3>
+        </span>
+        <p class={styles.description}>
+          Hashable is an open-source, client-side gateway for users to test out
+          cryptographic functions and hashing methods. We also support a REST
+          API which users can use to hash strings in their own website on
+          client-side Javascript Applications.
+        </p>
+
+        <div class={styles.actions}>
+          <button primary>Open Playground</button>
+
+          <button>Learn</button>
+        </div>
+      </header>
+
+      <hr />
+    </div>
+  );
+}
+
+export default App;
