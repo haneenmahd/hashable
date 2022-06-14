b20868ed505ae1e20899256ff33b9312e58752b7
Haneen Mahdin <haneenmahdin@gmail.com>
2022-06-14 22:43:37 +0530

    format

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {},
   },
