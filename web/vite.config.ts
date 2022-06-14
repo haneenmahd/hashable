c278637f4092ba0a13c5f283c9c4d31bfc5adafa
Haneen Mahdin <haneenmahdin@gmail.com>
2022-06-14 22:43:26 +0530

    Move localhost port to 3001

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
+  },
+  server: {
+    port: 3001,
   },
 });
