3cd5647acc777080eb2d08f45645c5ad7cd62c86
Haneen Mahdin <haneenmahdin@gmail.com>
2022-06-14 21:52:26 +0530

    Add quick actions toolbar in playground

--- web/src/pages/Playground.tsx
+++ web/src/pages/Playground.tsx
@@ -1,4 +1,5 @@
 import { Component } from "solid-js";
+import { Button } from "../components/Button";
 import { SidebarElement } from "../components/SidebarElement";
 
 const Sidebar: Component = () => (
@@ -15,9 +16,25 @@ const Sidebar: Component = () => (
   </div>
 );
 
+const QuickActions: Component = () => {
+  return (
+    <div class="absolute right-5 top-3 flex flex-row items-center">
+      <div class="mx-1">
+        <Button title="▶️ Run" primary />
+      </div>
+
+      <div class="mx-1">
+        <Button title="📜 Get code" />
+      </div>
+    </div>
+  );
+};
+
 const Main: Component = () => {
   return (
     <div class="w-[100%] h-[100vh] bg-white p-10 flex items-center justify-center">
+      <QuickActions />
+
       <input
         class="w-[100%] text-center text-6xl text-slate-800 outline-none focus:placeholder:text-slate-300"
         placeholder="Type string to hash...."
