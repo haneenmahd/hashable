65713135be9aebe6a63fa57cb0a3a72d9453d453
Haneen Mahdin <haneenmahdin@gmail.com>
2022-06-14 22:44:07 +0530

    Add editor and quick actions tool bar
    Also updated the sidebar with a new style

--- web/src/pages/Playground.tsx
+++ web/src/pages/Playground.tsx
@@ -1,26 +1,40 @@
-import { Component } from "solid-js";
+import axios from "axios";
+import { Accessor, Component, createSignal, Setter } from "solid-js";
 import { Button } from "../components/Button";
 import { SidebarElement } from "../components/SidebarElement";
 
 const Sidebar: Component = () => (
-  <div class="min-w-[16rem] h-[100vh] bg-slate-100">
-    <div class="py-2">
-      <SidebarElement text="Playground" noBorders />
-    </div>
+  <div class="p-5">
+    <div class="min-w-[16rem] h-[90vh] rounded-md ring-1 ring-slate-200 shadow-lg bg-slate-100">
+      <div class="py-2">
+        <SidebarElement text="Playground" noBorders />
+      </div>
 
-    <div class="py-0">
-      <SidebarElement text="Method" />
+      <div class="py-0">
+        <SidebarElement text="Method" />
 
-      <SidebarElement text="Encoding" />
+        <SidebarElement text="Encoding" />
+      </div>
     </div>
   </div>
 );
 
-const QuickActions: Component = () => {
+const QuickActions: Component<{
+  value: Accessor<string>;
+  setHash: Setter<string>;
+}> = ({ value, setHash }) => {
+  const fetchHash = async () => {
+    const response = await axios.get(
+      `http://localhost:3000/api/hash?algorithm=md5&str=${value()}&encoding=hex`
+    );
+
+    setHash(response.data);
+  };
+
   return (
     <div class="absolute right-5 top-3 flex flex-row items-center">
       <div class="mx-1">
-        <Button title="▶️ Run" primary />
+        <Button title="▶️ Run" primary action={fetchHash} />
       </div>
 
       <div class="mx-1">
@@ -31,14 +45,23 @@ const QuickActions: Component = () => {
 };
 
 const Main: Component = () => {
+  const [value, setValue] = createSignal("");
+  const [hash, setHash] = createSignal("01234567890");
+
   return (
-    <div class="w-[100%] h-[100vh] bg-white p-10 flex items-center justify-center">
-      <QuickActions />
+    <div class="w-[100%] max-w-[100%] h-[100vh] bg-white p-10 flex flex-col items-center justify-center">
+      <QuickActions value={value} setHash={setHash} />
 
       <input
-        class="w-[100%] text-center text-6xl text-slate-800 outline-none focus:placeholder:text-slate-300"
+        class="w-[100%] my-4 font-semibold text-center text-6xl text-slate-800 outline-none focus:placeholder:text-slate-300"
         placeholder="Type string to hash...."
+        value={value()}
+        onChange={(e) => setValue((e.target as HTMLInputElement).value)}
       />
+
+      <span class="my-4 font-semibold text-center slashed-zero text-3xl text-slate-500 outline-none focus:placeholder:text-slate-300">
+        <p>{hash()}</p>
+      </span>
     </div>
   );
 };
