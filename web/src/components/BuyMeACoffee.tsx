731bebdcef869e2caa50f7709ad1cd9aaae974b1
Haneen Mahdin <haneenmahdin@gmail.com>
2022-07-05 08:00:00 +0530

    add buy me a coffee button

---
+++ web/src/components/BuyMeACoffee.tsx
@@ -0,0 +1,13 @@
+import { Component } from "solid-js";
+
+const BuyMeACoffee: Component = () => (
+  <span class="my-5 mx-7 bg-yellow-400 hover:bg-yellow-500 w-fit px-4 py-3 rounded-md shadow-lg shadow-yellow-500/60 transition-colors">
+    <span class="font-medium text-yellow-900 inline-block">
+      <a href="https://www.buymeacoffee.com/haneenmahdin" target="_blank">
+        ☕️ Buy Me a Coffee
+      </a>
+    </span>
+  </span>
+);
+
+export default BuyMeACoffee;
